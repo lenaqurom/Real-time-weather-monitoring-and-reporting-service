712cd492e9b0617cf77d1a1fc18966219fb98284
Lina Qurom <you@example.com>
2025-03-22 00:19:17 +0200

    Implement SunBot, RainBot, and SnowBot classes

--- Services/Bots/SunBot.cs
+++ Services/Bots/SunBot.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherMonitoring.Interfaces;
+using WeatherMonitoring.Models;
 
 namespace WeatherMonitoring.Services.Bots
 {
-    internal class SunBot
+    public class SunBot : IWeatherBotObserver
     {
+        private readonly double _threshold;
+        private readonly string _message;
+
+        public SunBot(double threshold, string message)
+        {
+            _threshold = threshold;
+            _message = message;
+        }
+        public void Activate(WeatherData data)
+        {
+            if (data.Temperature < _threshold)
+            {
+                Console.WriteLine("SunBot activated");
+                Console.WriteLine(_message);
+            }
+        }
     }
 }
