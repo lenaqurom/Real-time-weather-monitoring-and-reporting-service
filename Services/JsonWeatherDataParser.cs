2198cea42dc896541505bd72470bc9bec72f2206
Lina Qurom <you@example.com>
2025-03-22 05:37:27 +0200

    Add JSON and XML parsers for weather data

---
+++ Services/JsonWeatherDataParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WeatherMonitoring.Interfaces;
+using WeatherMonitoring.Models;
+
+namespace WeatherMonitoring.Services
+{
+    public class JsonWeatherDataParser : IWeatherDataParser
+    {
+        public WeatherData Convert(string input)
+        {
+            return JsonSerializer.Deserialize<WeatherData>(input);
+        }
+    }
+}
---
+++ Services/XmlWeatherDataParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using WeatherMonitoring.Interfaces;
+using WeatherMonitoring.Models;
+
+namespace WeatherMonitoring.Services
+{
+    public class XmlWeatherDataParser : IWeatherDataParser
+    {
+        public WeatherData Convert(string input)
+        {
+            var xml = XElement.Parse(input);
+            return new WeatherData
+            {
+                Location = xml.Element("Location")?.Value,
+                Temperature = double.Parse(xml.Element("Temperature")?.Value ?? "0"),
+                Humidity = double.Parse(xml.Element("Humidity")?.Value ?? "0")
+            };
+        }
+    }
+}
